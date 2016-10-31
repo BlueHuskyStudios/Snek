a4d9e7c97480d79ea72420de1df692a7f92a8c61
Kyli Rouge <Supuhstar0@Gmail.com>
2016-10-31 19:32:52 -0400

    Started IntelliJ Project

---
+++ JVM/src/org/bh/game/snek/Main.kt
@@ -0,0 +1,8 @@
+package org.bh.game.snek
+
+/**
+ * The entry point to Snek
+ */
+fun main(args: Array<String>) {
+    print("Sup")
+}
---
+++ JVM/src/org/bh/game/snek/state/SnekGameState.kt
@@ -0,0 +1,16 @@
+package org.bh.game.snek.state
+
+/**
+ * Created by Coding on 2016-10-30.
+ */
+class SnekGameState {
+    val snek: SnekDataView
+}
+
+class SnekDataView {
+
+}
+
+data class SnekData(val points: Array<Point>) {
+
+}
