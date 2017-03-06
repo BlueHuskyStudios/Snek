e2f65cc876bb9e76a5e408d916f00ea975c85739
Ben Leggiero <Ben@BHStudios.org>
2017-03-05 20:58:58 -0500

    Removed redundant equals and hashcode

--- JVM/src/org/bh/game/snek/state/SnekData.kt
+++ JVM/src/org/bh/game/snek/state/SnekData.kt
@@ -44,29 +44,6 @@ data class SnekData(
             snekPath = change.snekPath ?: this.snekPath,
             debug = change.debug ?: this.debug)
 
-
-    @Suppress("RedundantIf")
-    override fun equals(other: Any?): Boolean
-            = if (this === other) true
-            else if (other !is SnekData) false
-            else if (other.boardSize != this.boardSize) false
-            else if (other.snekPath != this.snekPath) false
-            else if (other.leaderboard != this.leaderboard) false
-            else if (other.screen != this.screen) false
-            else if (other.apple != this.apple) false
-            else if (other.debug != this.debug) false
-            else true
-
-
-    override fun hashCode(): Int {
-        var result = boardSize.hashCode()
-        result = 31 * result + (snekPath.hashCode())
-        result = 31 * result + (leaderboard.hashCode())
-        result = 31 * result + (screen.hashCode())
-        result = 31 * result + (apple.hashCode())
-        return result
-    }
-
     companion object
 }
 
@@ -89,6 +66,12 @@ data class SnekDataChange(
         val debug: Boolean? = null)
     : DataView<SnekDataChange>, Data, StateChange<SnekDataChange, SnekData> {
 
+    /**
+     * In this case, this change is its own data. So, this just returns `this`
+     */
+    override val data: SnekDataChange get() = this
+
+
     constructor(exactly: BaseSnekDataView) : this(
             boardSize = exactly.boardSize,
             snekPath = exactly.path,
@@ -96,6 +79,7 @@ data class SnekDataChange(
             screen = exactly.screen,
             apple = exactly.apple,
             debug = exactly.debug)
+
 
     constructor(exactly: SnekData) : this(
             boardSize = exactly.boardSize,
@@ -105,40 +89,6 @@ data class SnekDataChange(
             apple = exactly.apple,
             debug = exactly.debug)
 
-    /**
-     * In this case, this change is its own data. So, this just returns `this`
-     */
-    override val data: SnekDataChange get() = this
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is SnekDataChange) return false
-        if (other.boardSize != this.boardSize) return false
-        if (other.snekPath != null) {
-            if (this.snekPath != null) {
-                if (other.snekPath != this.snekPath) return false
-            } else {
-                return false
-            }
-        } else if (this.snekPath != null) { // && other.snekPath == null
-            return false
-        }
-        if (other.leaderboard != this.leaderboard) return false
-        if (other.screen != this.screen) return false
-        if (other.apple != this.apple) return false
-        if (other.debug != this.debug) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = boardSize?.hashCode() ?: 0
-        result = 31 * result + (snekPath?.hashCode() ?: 0)
-        result = 31 * result + (leaderboard?.hashCode() ?: 0)
-        result = 31 * result + (screen?.hashCode() ?: 0)
-        result = 31 * result + (apple?.hashCode() ?: 0)
-        result = 31 * result + (debug?.hashCode() ?: 0)
-        return result
-    }
 
     override fun applyingChange(change: SnekDataChange): SnekDataChange
             = SnekDataChange(
